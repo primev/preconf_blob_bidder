12ea8795786482698724a5fd8780cc04d205e7fb
Evan K <ekcopersonal@gmail.com>
2024-06-30 16:05:44 -0400

    feat: add geth client and account authentication

--- core/mevcommit/client.go
+++ core/mevcommit/client.go
@@ -1,28 +1,50 @@
 package mevcommit
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
 	"log"
-	"os"
 
 	pb "github.com/primev/mev-commit/p2p/gen/go/bidderapi/v1"
 	"google.golang.org/grpc"
 
-	"fmt"
-
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 )
 
-// Config holds the configuration settings.
-type Config struct {
+// BidderConfig holds the configuration settings for mev-commit bidder node.
+type BidderConfig struct {
 	ServerAddress string `json:"server_address" yaml:"server_address"`
 	LogFmt        string `json:"log_fmt" yaml:"log_fmt"`
 	LogLevel      string `json:"log_level" yaml:"log_level"`
 }
 
-// NewBidClient creates a new gRPC client connection to the bidder service and returns a bidder instance.
-func NewBidClient(cfg Config) (*bidder, error) {
+// Bidder utilizes the mevcommit bidder client to interact with the mevcommit chain.
+type Bidder struct {
+	client pb.BidderClient
+}
+
+// GethConfig holds configuration settings for a geth node to connect to mev-commit chain.
+type GethConfig struct {
+	Endpoint string `json:"endpoint" yaml:"endpoint"`
+}
+
+// AuthAcct holds the private key, public key, address, and authentication.
+type AuthAcct struct {
+	PrivateKey *ecdsa.PrivateKey
+	PublicKey  *ecdsa.PublicKey
+	Address    common.Address
+	Auth *bind.TransactOpts
+}
+
+// NewBidderClient creates a new gRPC client connection to the bidder service and returns a bidder instance.
+func NewBidderClient(cfg BidderConfig) (*Bidder, error) {
 	conn, err := grpc.NewClient(cfg.ServerAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		fmt.Printf("Failed to connect to gRPC server: %v", err)
@@ -30,25 +52,57 @@ func NewBidClient(cfg Config) (*bidder, error) {
 	}
 
 	client := pb.NewBidderClient(conn)
-	return &bidder{client: client}, nil
+	return &Bidder{client: client}, nil
 }
 
-// connect to mev-commit chain
-func NewMevCommitClient(endpoint string) (*ethclient.Client, error) {
+// NewGethClient connects to the MEV-Commit chain given an endpoint.
+func NewGethClient(endpoint string) (*ethclient.Client, error) {
 	client, err := rpc.Dial(endpoint)
 	if err != nil {
 		log.Println(err)
+		return nil, err
 	}
 
 	ec := ethclient.NewClient(client)
 	return ec, nil
 }
 
-// LoadABI loads the ABI from the specified file path
-func LoadABI(filePath string) (string, error) {
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return "", err
+// AuthenticateAddress converts a hex-encoded private key string to a AuthAcct struct.
+func AuthenticateAddress(privateKeyHex string, client *ethclient.Client) (*AuthAcct, error) {
+	if privateKeyHex == "" {
+		return nil, nil
 	}
-	return string(data), nil
+
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		log.Printf("Failed to load private key: %v", err)
+		return nil, err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Failed to assert public key type")
+	}
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get chain ID: %v", err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+
+	return &AuthAcct{
+		PrivateKey: privateKey,
+		PublicKey:  publicKeyECDSA,
+		Address:    address,
+		// Nonce:      nonce,
+		Auth:       auth,
+	}, nil
 }
