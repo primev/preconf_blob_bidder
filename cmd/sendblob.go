3979a3e7c8233bf4cbc06840d095f017e8dd3ead
Evan K <ekcopersonal@gmail.com>
2024-07-16 19:55:35 -0400

    feat: add blob preconf

---
+++ cmd/sendblob.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"time"
+
+	ee "github.com/primev/preconf_blob_bidder/core/eth"
+	bb "github.com/primev/preconf_blob_bidder/core/mevcommit"
+)
+
+func main() {
+
+	// Start mevcommit bidder node client
+	cfg := bb.BidderConfig{
+		ServerAddress: "localhost:13524", // Default address for mevcommit gRPC server
+		LogFmt:        "json",            // Example log format
+		LogLevel:      "info",            // Example log level
+	}
+
+	bidderClient, err := bb.NewBidderClient(cfg)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v. Remember to connect to the mev-commit p2p bidder node.", err)
+	}
+	fmt.Println("Connected to mev-commit client")
+
+	// Start Holesky client with command line flags
+	endpoint := flag.String("endpoint", "", "The Ethereum client endpoint")
+	privateKeyHex := flag.String("privatekey", "", "The private key in hex format")
+	flag.Parse()
+	if *endpoint == "" {
+		log.Fatal("Endpoint is required. Use the -endpoint flag to provide it.")
+	}
+
+	client, err := bb.NewGethClient(*endpoint)
+	if err != nil {
+		log.Fatalf("Failed to connect to client: %v", err)
+	}
+
+	// Authenticate address with private key
+	authAcct, err := bb.AuthenticateAddress(*privateKeyHex, client)
+	if err != nil {
+		log.Fatalf("Failed to authenticate private key: %v", err)
+	}
+
+	// Get current block number
+	blockNumber, err := client.BlockNumber(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to retrieve block number: %v", err)
+	}
+	fmt.Printf("Current block number: %v\n", blockNumber)
+
+	// // Execute Blob Transaction
+	// txHash, err := ee.ExecuteBlobTransaction(client, *authAcct, 2)
+	// if err != nil {
+	// 	log.Fatalf("Failed to execute blob transaction: %v", err)
+	// }
+
+	// Send ETH Transfer
+	txHash, err := ee.SelfETHTransfer(client, *authAcct, big.NewInt(100000), 3000000, []byte{0x4c, 0xdc, 0xeb, 0x20})
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+	}
+
+	log.Printf("tx sent: %s", txHash)
+
+	// Convert uint64 to int64. Add +1 to be the next block number
+	blockNumberInt64 := int64(blockNumber) + 1
+	// print the preconf block number
+	fmt.Printf("Preconf block number: %v\n", blockNumberInt64)
+	currentTime := time.Now().UnixMilli()
+	// bid preconf parameters
+	txHashes := []string{strings.TrimPrefix(txHash, "0x")}
+	amount := "10000000000000" // Specify amount in wei
+	decayStart := currentTime - (time.Duration(8 * time.Second).Milliseconds())
+	decayEnd := currentTime + (time.Duration(8 * time.Second).Milliseconds())
+
+	response, err := bidderClient.SendBid(txHashes, amount, blockNumberInt64, decayStart, decayEnd)
+	if err != nil {
+		log.Fatalf("Failed to send bid: %v", err)
+	}
+
+	fmt.Printf("Bid response: %v\n", response)
+
+	// 7/10/24 - 0.4.0 release has auto deposits so this code is no longer needed.
+	// After preconf bid is sent and confirmed, wait 11 minutes and then withdraw the funds.
+	// time.Sleep(11 * time.Minute)
+
+	// // Withdraw the amount from the window
+	// err = bidderClient.WithdrawFunds(windowNumber)
+	// if err != nil {
+	// 	log.Fatalf("Failed to withdraw funds: %v", err)
+	// }
+}
--- core/eth/sendtx.go
+++ core/eth/sendtx.go
@@ -3,11 +3,29 @@ package eth
 import (
 	"bytes"
 	"context"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"fmt"
 	"math/big"
+	"os"
 
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
+	gokzg4844 "github.com/crate-crypto/go-kzg-4844"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/kzg4844"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/holiman/uint256"
 	bb "github.com/primev/preconf_blob_bidder/core/mevcommit"
+)
+
+const (
+	escalateMultiplier = 2
+	// numBlobs           = 2 // Set the number of blobs here
 )
 
 // send an eth transfer to self
@@ -69,3 +87,153 @@ func SelfETHTransfer(client *ethclient.Client, authAcct bb.AuthAcct, value *big.
 	return signedTx.Hash().Hex(), nil
 }
 
+// ExecuteBlobTransaction executes a blob transaction and returns the transaction hash
+func ExecuteBlobTransaction(client *ethclient.Client, authAcct bb.AuthAcct, numBlobs int) (string, error) {
+	glogger := log.NewGlogHandler(log.NewTerminalHandler(os.Stderr, true))
+	glogger.Verbosity(log.LevelInfo)
+	log.SetDefault(log.NewLogger(glogger))
+
+	privateKey := authAcct.PrivateKey
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return "", fmt.Errorf("failed to cast public key to ECDSA")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", err
+	}
+
+	gasTipCap, err := client.SuggestGasTipCap(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	gasFeeCap, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", err
+	}
+
+	gasLimit, err := client.EstimateGas(context.Background(),
+		ethereum.CallMsg{
+			From:      fromAddress,
+			To:        &fromAddress,
+			GasFeeCap: gasFeeCap,
+			GasTipCap: gasTipCap,
+			Value:     big.NewInt(0),
+		})
+	if err != nil {
+		return "", err
+	}
+
+	// Estimate pending block's blobFeeCap.
+	parentHeader, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		return "", err
+	}
+	parentExcessBlobGas := eip4844.CalcExcessBlobGas(*parentHeader.ExcessBlobGas, *parentHeader.BlobGasUsed)
+	blobFeeCap := eip4844.CalcBlobFee(parentExcessBlobGas)
+
+	log.Info("blob gas info", "excessBlobGas", parentExcessBlobGas, "blobFeeCap", blobFeeCap)
+
+	blobs := randBlobs(numBlobs)
+	sideCar := makeSidecar(blobs)
+	blobHashes := sideCar.BlobHashes()
+
+	tx := types.NewTx(&types.BlobTx{
+		ChainID:    uint256.MustFromBig(chainID),
+		Nonce:      nonce,
+		GasTipCap:  uint256.MustFromBig(gasTipCap),
+		GasFeeCap:  uint256.MustFromBig(gasFeeCap),
+		Gas:        gasLimit * 12 / 10,
+		To:         fromAddress,
+		BlobFeeCap: uint256.MustFromBig(blobFeeCap),
+		BlobHashes: blobHashes,
+		Sidecar:    sideCar,
+	})
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		return "", err
+	}
+
+	signedTx, err := auth.Signer(auth.From, tx)
+	if err != nil {
+		return "", err
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return "", err
+	}
+
+	log.Info("transaction parameters",
+		"hash", signedTx.Hash().String(),
+		"chainID", signedTx.ChainId(),
+		"nonce", signedTx.Nonce(),
+		"gasTipCap", signedTx.GasTipCap(),
+		"gasFeeCap", signedTx.GasFeeCap(),
+		"gasLimit", signedTx.Gas(),
+		"to", signedTx.To(),
+		"blobFeeCap", signedTx.BlobGasFeeCap(),
+		"blobHashes", signedTx.BlobHashes())
+
+	return signedTx.Hash().String(), nil
+}
+
+func makeSidecar(blobs []kzg4844.Blob) *types.BlobTxSidecar {
+	var (
+		commitments []kzg4844.Commitment
+		proofs      []kzg4844.Proof
+	)
+
+	for _, blob := range blobs {
+		c, _ := kzg4844.BlobToCommitment(&blob)
+		p, _ := kzg4844.ComputeBlobProof(&blob, c)
+
+		commitments = append(commitments, c)
+		proofs = append(proofs, p)
+	}
+
+	return &types.BlobTxSidecar{
+		Blobs:       blobs,
+		Commitments: commitments,
+		Proofs:      proofs,
+	}
+}
+
+func randBlobs(n int) []kzg4844.Blob {
+	blobs := make([]kzg4844.Blob, n)
+	for i := 0; i < n; i++ {
+		blobs[i] = randBlob()
+	}
+	return blobs
+}
+
+func randBlob() kzg4844.Blob {
+	var blob kzg4844.Blob
+	for i := 0; i < len(blob); i += gokzg4844.SerializedScalarSize {
+		fieldElementBytes := randFieldElement()
+		copy(blob[i:i+gokzg4844.SerializedScalarSize], fieldElementBytes[:])
+	}
+	return blob
+}
+
+func randFieldElement() [32]byte {
+	bytes := make([]byte, 32)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic("failed to get random field element")
+	}
+	var r fr.Element
+	r.SetBytes(bytes)
+
+	return gokzg4844.SerializeScalar(r)
+}
